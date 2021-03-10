606e06617517a02cb229c3aef8c9c20f179729af
sound shell <sound_shell@outlook.com>
2021-03-10 20:31:45 +0800

    Fix unit test run error.

--- test/error.test.ts
+++ test/error.test.ts
@@ -4,9 +4,9 @@ import { handleError } from '../src/error'
 describe('test/error.test.ts', () => {
   it('Should be the result of handleError.', async () => {
     try {
-      handleError('error')
+      handleError('Internal service error.')
     } catch (error) {
-      assert(error.message === 'error')
+      assert(error.message === 'Internal service error.')
     }
   })
 })
--- test/validate.test.ts
+++ test/validate.test.ts
@@ -4,14 +4,14 @@ import { validateId } from '../src/validate'
 describe('test/validate.test.ts', () => {
   it('Should be the result of validateId.', async () => {
     const result = validateId({
-      id: BigInt(3),
-      maxId: BigInt(2),
+      id: BigInt(32),
+      maxId: BigInt(31),
       errorMessage:
         'The data center id cannot be greater than ${maxId} or less than 0.'
     })
 
     assert(
-      result === 'The data center id cannot be greater than 2 or less than 0.'
+      result === 'The data center id cannot be greater than 31 or less than 0.'
     )
   })
 })

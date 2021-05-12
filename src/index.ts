7757b233a08c32d8545bba7986c58284886817bc
sound shell <sound_shell@outlook.com>
2021-05-13 00:25:26 +0800

    test:(all) Adding tests

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ export * from './interface/id.interface';
 export * from './interface/snowflake.interface';
 export * from './interface/timestamp.interface';
 export * from './interface/validate.interface';
-export * from './snowflake';
 export * from './timestamp';
 export * from './validate';
+export {snowflake};
+import {snowflake as relSnowflake} from './snowflake';
+
+const snowflake = relSnowflake;

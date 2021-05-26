89d259b3e38297e107df0d596cfef546e5557c1f
sound shell <sound_shell@outlook.com>
2021-05-26 17:09:07 +0800

    test:(snowflake.test.ts) Add test.

--- test/snowflake.test.ts
+++ test/snowflake.test.ts
@@ -1,18 +1,20 @@
 import * as assert from 'assert';
 import {snowflake} from '../src/snowflake';
 
-describe('test/snowflake.test.ts', () => {
-  it('should be the snowflake algorithm', async () => {
-    const result = snowflake({twEpoch: Date.now()});
+let SNOWFLAKE!: () => string;
 
-    assert(typeof result === 'function');
+describe('test/snowflake.test.ts', () => {
+  before(async () => {
+    SNOWFLAKE = snowflake({twEpoch: Date.now()});
+  });
+
+  it('should be the snowflake algorithm', async () => {
+    assert(typeof SNOWFLAKE === 'function');
   });
 
   it('should be the snowflake algorithm does not repeat', async () => {
-    const generateId = snowflake({twEpoch: Date.now()});
-
     assert(
-      [...new Set([...new Array(200000).keys()].map(() => generateId()))]
+      [...new Set([...new Array(200000).keys()].map(() => SNOWFLAKE()))]
         .length === 200000
     );
   });

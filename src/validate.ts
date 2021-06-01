591cea8642530bb0298011e86d1ed16dacfaf5cd
sound shell <sound_shell@outlook.com>
2021-06-01 14:20:37 +0800

    feat:(validate.ts) 1.2.4

--- src/validate.ts
+++ src/validate.ts
@@ -1,9 +1,9 @@
 import {ValidateId} from './interface/validate.interface';
 
 export const validateId: ValidateId = ({id, maxId, message}) => {
-  const errorId = id > maxId || id < 0;
+  const isErrorId = id > maxId || id < 0;
 
-  if (errorId) {
+  if (isErrorId) {
     throw new Error(message.replace('${maxId}', maxId.toString()));
   }
 };
